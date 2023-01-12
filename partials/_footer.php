90eb9d983767290b65cda63187a8fa4d6398996f
Natalia FABIANO <natalia.fabiano@hotmail.com>
2023-01-12 16:09:38 +0100

    modif bio/header/bar/footer

--- partials/_footer.php
+++ partials/_footer.php
@@ -1,12 +1,39 @@
-    <!-----------------Footer----------------->
-    <div class="footer">
-        <div class="container links">
-            <p>lalal</p>
-        </div>
-        <p class="mt-2 text-capitalize">© <?php echo  date("Y") ?> Copyright: <a class="a-footer" href="https://appryll.github.io/Portfolio-NataliaFabiano/"><<|NF|>></a>- Toute reproduction interdite sans l'autorisation de l'auteur -</p>
-    </div>
-    <!-----------------Fin Footer----------------->
 
+    <div class="footer" id="links">
+        <div class="container links d-flex align-items-center justify-content-around">
+
+            <!-- Mail -->
+            <a class="btn btn-link btn-floating btn-lg text-dark"
+                    href="mailto:nataliafabiano.dev@gmail.com.com" role="button"
+                    data-mdb-ripple-color="dark" title="nataliafabiano.dev@gmail.com"><i
+                            class="fa fa-envelope" aria-hidden="true"></i></a>
+
+            <!-- Telegram -->
+            <a class="btn btn-link btn-floating btn-lg text-dark" href="https://t.me/NatyAvril"
+                    role="button" data-mdb-ripple-color="dark" target="_black" title="Telegram"><i
+                            class="fab fa-telegram"></i></a>
+
+            <!-- Twitter -->
+            <a class="btn btn-link btn-floating btn-lg text-dark"
+                    href="https://twitter.com/_Appryll_" role="button" data-mdb-ripple-color="dark"
+                    target="_black" title="Twitter"><i class="fab fa-twitter"></i></a>
+
+            <!-- Linkedin -->
+            <a class="btn btn-link btn-floating btn-lg text-dark"
+                    href="https://www.linkedin.com/in/natalia-fabiano/?locale=fr_FR" role="button"
+                    data-mdb-ripple-color="dark" target="_black" title="Linkedin"><i
+                            class="fab fa-linkedin"></i></a>
+
+            <!-- Github -->
+            <a class="btn btn-link btn-floating btn-lg text-dark" href="https://github.com/Appryll"
+                    role="button" data-mdb-ripple-color="dark" target="_black" title="GitHub"><i
+                            class="fab fa-github"></i></a>
+            
+        </div>
+        <p class="copy-footer mt-2 text-capitalize">© <?php echo  date("Y") ?> Copyright: <a href="https://appryll.github.io/Portfolio-NataliaFabiano/"><<|NF|>></a>- Toute reproduction interdite sans l'autorisation de l'auteur -</p>
+
+    </div>
+    
     <!-- Bootstrap.JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
--- partials/_menu.php
+++ partials/_menu.php
@@ -7,8 +7,13 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- CSS -->
     <link rel="stylesheet" href="css/style.css">
-
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Cinzel&family=Moon+Dance&family=Playfair+Display&display=swap" rel="stylesheet">
+    
     <title>Portfolio Natalia FABIANO</title>
 </head>
 <body>
@@ -22,7 +27,7 @@
         <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active mx-4" aria-current="page" href="#">À propos de moi</a>
+                    <a class="nav-link active mx-4" aria-current="page" href="#bio">À propos de moi</a>
                 </li>
                 <li class="nav-item mx-4">
                     <a class="nav-link" href="#">Formation</a>
@@ -31,7 +36,7 @@
                     <a class="nav-link" href="#">Mes Projets</a>
                 </li>
                 <li class="nav-item mx-4">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contact</a>
+                    <a class="nav-link" href="#links" tabindex="-1" aria-disabled="true">Contact</a>
                 </li>
             </ul>
         </div>
